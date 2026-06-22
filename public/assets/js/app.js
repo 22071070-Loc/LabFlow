f6280e51603bcbf87d46b9aa0f2f1c7605dba00f
maingocgiap <giapdabong@gmail.com>
2026-06-23 04:26:15 +0700

    Delete public directory

--- public/assets/js/app.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('.alert').forEach((alert) => {
-    setTimeout(() => alert.style.opacity = '0.92', 2000);
-  });
-});
