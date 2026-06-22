70126b540822f686310b2e60a07a87be4187bc5c
22071070-Loc <22071070@vnu.edu.vn>
2026-06-23 02:58:55 +0700

    Delete app.js

--- assets/js/app.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('click', function (event) {
-  const target = event.target.closest('[data-confirm]');
-  if (target && !confirm(target.getAttribute('data-confirm'))) {
-    event.preventDefault();
-  }
-});
